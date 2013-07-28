f7918bd2e3114cb10f5e7f1a654c77cf23a9d94a
Stephan Plepelits <skunk@xover.mud.at>
2013-07-28 16:32:52 +0200

    Add real-world data test

--- load.sh
+++ load.sh
@@ -21,3 +21,5 @@ psql $@ -c "select pgmapcss_install('test', \$\$$FILE\$\$);"
 
 psql $@ -c "select * from css_check_test('N1234', 'highway=>primary, name=>Testroad', null, Array['way','line'], 25000);"
 psql $@ -c "select * from css_check_test('N1234', 'highway=>primary, bridge=>yes, name=>Testroad', null, Array['way','line'], 25000);"
+
+psql $@ -c "select (css_check_test(cast(osm_id as text), tags, way, Array['way','line'], 25000)).*, osm_id, tags, way from planet_osm_line;"
