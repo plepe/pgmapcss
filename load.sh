fd91d8f2dafe944bacc6e0375ed38db58c90314b
Stephan Plepelits <skunk@xover.mud.at>
2013-08-02 07:57:23 +0200

    load.sh: Abort on compilation error

--- load.sh
+++ load.sh
@@ -2,6 +2,10 @@
 
 echo "=== Resulting function ===" > test.output
 FILE=`cat test.mapcss`
-psql $@ -P format=unaligned -c "select pgmapcss_install('test', \$\$$FILE\$\$);" | tail -n+2 | head -n-2 >> test.output
+psql $@ --set ON_ERROR_STOP=1 -P format=unaligned -c "select pgmapcss_install('test', \$\$$FILE\$\$);" | tail -n+2 | head -n-2 >> test.output
 
-less test.output
+if [ $? -eq 0 ] ; then
+  less test.output
+else
+  echo "An error occured when compiling CSS file"
+fi
