6e28e3c7b75c1bfff1e6ae5a3eeddfb2bb2d06da
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-08-11 14:24:04 +0200

    Eval::eval_any(): return first non-null value

--- src/eval.sql
+++ src/eval.sql
@@ -285,6 +285,22 @@ begin
 end;
 $$ language 'plpgsql' immutable;
 
+create or replace function eval_any(param text[],
+  object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
+returns text
+as $$
+#variable_conflict use_variable
+declare
+  r text;
+begin
+  foreach r in array param loop
+    if r is not null and r != '' then
+      return r;
+    end if;
+  end loop;
+end;
+$$ language 'plpgsql' immutable;
+
 create or replace function eval_cond(param text[],
   object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
 returns text
