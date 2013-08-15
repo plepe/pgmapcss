46b05dd23fdb54a3d41745440c8f064d0218622f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-08-15 09:51:29 +0200

    Eval::debug(): raise notice

--- src/eval.sql
+++ src/eval.sql
@@ -680,6 +680,19 @@ begin
 end;
 $$ language 'plpgsql' immutable;
 
+create or replace function eval_debug(param text[],
+  object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
+returns text
+as $$
+#variable_conflict use_variable
+declare
+begin
+  raise notice '%', param[1];
+
+  return param[1];
+end;
+$$ language 'plpgsql' immutable;
+
 create or replace function eval_TEMPLATE(param text[],
   object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
 returns text
