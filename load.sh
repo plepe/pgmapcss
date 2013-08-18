e3a53541c9f406487d19dc30170fe48af17da2a1
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-08-18 22:05:18 +0200

    preprocess.pl: rename from preprocess_colors.pl

--- load.sh
+++ load.sh
@@ -5,6 +5,8 @@ FILE=`cat test.mapcss`
 psql $@ --set ON_ERROR_STOP=1 -P format=unaligned -c "select pgmapcss_install('test', \$\$$FILE\$\$);" | tail -n+2 | head -n-2 >> test.output
 ERR=$?
 
+./preprocess.pl
+
 if [ $ERR -eq 0 ] ; then
   less test.output
 else
@@ -12,4 +14,3 @@ else
   exit $ERR
 fi
 
-./preprocess_colors.pl
