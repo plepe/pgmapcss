6da0cf5792f8b15ffe0ab47cae8023d629e4f16f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-08-22 03:42:48 +0200

    Eval:: Implement min() and max()

--- src/eval.sql
+++ src/eval.sql
@@ -744,3 +744,59 @@ declare
 begin
 end;
 $$ language 'plpgsql' immutable;
+
+create or replace function eval_min(param text[],
+  object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
+returns text
+as $$
+#variable_conflict use_variable
+declare
+  r text;
+  f float;
+  min float;
+begin
+  foreach r in array param loop
+    r := eval_number(Array[r], object, current, render_context);
+
+    if r is not null and r != '' then
+      f := cast(r as float);
+
+      if min is null or f < min then
+	min := f;
+      end if;
+    end if;
+  end loop;
+
+  return coalesce(cast(min as text), '');
+end;
+$$ language 'plpgsql' immutable;
+
+
+
+create or replace function eval_max(param text[],
+  object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
+returns text
+as $$
+#variable_conflict use_variable
+declare
+  r text;
+  f float;
+  max float;
+begin
+  foreach r in array param loop
+    r := eval_number(Array[r], object, current, render_context);
+
+    if r is not null and r != '' then
+      f := cast(r as float);
+
+      if max is null or f > max then
+	max := f;
+      end if;
+    end if;
+  end loop;
+
+  return coalesce(cast(max as text), '');
+end;
+$$ language 'plpgsql' immutable;
+
+
