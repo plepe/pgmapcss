1a62a3afbf1c4b33b74a2d5d931de40562cf12ed
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-08-25 07:57:55 +0200

    Eval: new list functions unique() and sort()

--- src/eval.sql
+++ src/eval.sql
@@ -818,3 +818,45 @@ begin
   return null;
 end;
 $$ language 'plpgsql' immutable;
+
+create or replace function eval_unique(param text[],
+  object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
+returns text
+as $$
+#variable_conflict use_variable
+declare
+  list text[];
+  i int;
+begin
+  if array_upper(param, 1) < 1 then
+    return '';
+  end if;
+
+  list := string_to_array(param[1], ';');
+
+  select array_agg(el) into list from (select unnest(list) el group by el) t;
+
+  return array_to_string(list, ';');
+end;
+$$ language 'plpgsql' immutable;
+
+create or replace function eval_sort(param text[],
+  object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
+returns text
+as $$
+#variable_conflict use_variable
+declare
+  list text[];
+  i int;
+begin
+  if array_upper(param, 1) < 1 then
+    return '';
+  end if;
+
+  list := string_to_array(param[1], ';');
+
+  select array_agg(el) into list from (select unnest(list) el order by el) t;
+
+  return array_to_string(list, ';');
+end;
+$$ language 'plpgsql' immutable;
