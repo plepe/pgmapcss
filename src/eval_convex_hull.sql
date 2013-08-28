eed2d9388a66c73fb50afe28a095b12eb851133f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-08-28 23:07:16 +0200

    Eval::convex_hull: new function

---
+++ src/eval_convex_hull.sql
@@ -0,0 +1,15 @@
+create or replace function eval_convex_hull(param text[],
+  object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
+returns text
+as $$
+#variable_conflict use_variable
+declare
+  t text;
+begin
+  if array_upper(param, 1) < 1 then
+    return '';
+  end if;
+
+  return ST_ConvexHull(param[1]);
+end;
+$$ language 'plpgsql' immutable;
