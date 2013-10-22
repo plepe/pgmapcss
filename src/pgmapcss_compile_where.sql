31ef40a6b2f7a802891af8e2a15551a991e5926f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-10-22 22:23:28 +0200

    pgmapcss_compile_where: use @style_element_property for list of props

--- src/pgmapcss_compile_where.sql
+++ src/pgmapcss_compile_where.sql
@@ -22,7 +22,7 @@ declare
 begin
   for r in select unnest(stat.selectors) selectors, unnest(stat.properties) properties loop
     -- TODO: make list of match-relevant tags configurable
-    if (r.properties).prop_has_value ?| Array['text', 'width', 'fill-color', 'icon-image', 'fill-image', 'image'] then
+    if (r.properties).prop_has_value ?| (select array_agg(x) from (select unnest(cast((each(stat.prop_style_element)).value as text[])) x) t) then
       where_selectors := array_append(where_selectors, r.selectors);
 
     elsif (r.properties).has_combine then
