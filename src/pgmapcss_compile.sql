f12fa47f1b5d7e2049fa4ce9a5be5ddf8dad1dbc
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-10-30 14:30:32 +0100

    pgmapcss_compile: Bugfix, set current pseudo_element_ind on final checks

--- src/pgmapcss_compile.sql
+++ src/pgmapcss_compile.sql
@@ -65,6 +65,7 @@ begin
   ret = ret || E'  ret.tags=current.tags;\n';
   ret = ret || E'  for r in select * from (select generate_series(1, ' || array_upper(stat.pseudo_elements, 1) || E') i, unnest(current.styles) style) t order by coalesce(cast(style->''object-z-index'' as float), 0) asc loop\n';
   ret = ret || E'    if current.has_pseudo_element[r.i] then\n';
+  ret = ret || E'      current.pseudo_element_ind = r.i;\n';
 
   for i in select dep.key main_key, prop.key "key", has_values.value from each(stat.prop_depend) dep right join each(stat.properties_values) prop on prop.key = any(cast(dep.value as text[])) left join each(stat.properties_values) has_values on dep.key=has_values.key where has_values.value != '{NULL}' or has_values.value is null order by dep.key loop
     if current_main_prop != i.main_key or
