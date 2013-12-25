1f537f896d87a183b0b7e2ecf8d8653558e575dc
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-12-25 13:31:10 +0100

    python-reimpl: detect end-of-file

--- parser/parse_file.py
+++ parser/parse_file.py
@@ -1,12 +1,14 @@
 from .parse_selectors import parse_selectors
 from .parse_properties import parse_properties
+import re
 
 def parse_file(stat, file):
     content = open(file).read()
     to_parse = content
     stat['statements'] = []
 
-    while(to_parse):
+# read statements until there's only whitespace left
+    while not re.match('\s*$', to_parse):
         selectors = []
         to_parse = parse_selectors(selectors, to_parse)
 
