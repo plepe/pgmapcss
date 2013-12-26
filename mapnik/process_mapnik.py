341536c033da4b131b4a838b26637510f8b0dbff
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-12-26 07:40:15 +0100

    python-reimpl: mapnik: replacement patterns for style id and db parameters

--- mapnik/process_mapnik.py
+++ mapnik/process_mapnik.py
@@ -1,12 +1,22 @@
-def process_mapnik(style_id, stat):
+def process_mapnik(style_id, args, stat):
     f1 = open('mapnik/mapnik-2.0.mapnik', 'r')
     f2 = open(style_id + '.mapnik', 'w')
+
+    replacement = {
+        'style_id': id,
+        'host': args.host,
+        'password': args.password,
+        'database': args.database,
+        'user': args.user
+    }
 
     while True:
         r = f1.readline()
 
         if not r:
             break;
+
+        r = r.format(**replacement)
 
         f2.write(r)
 
