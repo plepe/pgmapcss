af1e7ae6780ecaa11b13652fa678ba3b5ec53bd6
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-12-26 09:09:24 +0100

    python-reimpl: process mapnik: canvas properties

--- mapnik/process_mapnik.py
+++ mapnik/process_mapnik.py
@@ -1,14 +1,26 @@
-def process_mapnik(style_id, args, stat):
+import pghstore
+
+def process_mapnik(style_id, args, stat, db):
     f1 = open('mapnik/mapnik-2.0.mapnik', 'r')
     f2 = open(style_id + '.mapnik', 'w')
 
     replacement = {
-        'style_id': id,
+        'style_id': style_id,
         'host': args.host,
         'password': args.password,
         'database': args.database,
         'user': args.user
     }
+
+    print("select * from {style_id}_check(pgmapcss_object('', '', null, Array['canvas']), pgmapcss_render_context(null, null))".format(**replacement))
+    res = db.prepare("select * from {style_id}_check(pgmapcss_object('', '', null, Array['canvas']), pgmapcss_render_context(null, null))".format(**replacement))
+    result = res()
+    if len(result) > 0:
+        canvas_properties = result[0][res.column_names.index('properties')]
+        canvas_properties = pghstore.loads(canvas_properties)
+
+        for (k, v) in canvas_properties.items():
+            replacement['canvas|' + k] = v or ''
 
     while True:
         r = f1.readline()
