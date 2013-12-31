12d282bab65d23f5c1201abb88048dd6826df002
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2013-12-31 06:39:47 +0100

    python-reimpl: handle @import

---
+++ parser/parse_defines.py
@@ -0,0 +1,19 @@
+from .parse_types import *
+from .strip_comments import strip_comments
+import re
+
+def parse_defines(defines, to_parse):
+    while True:
+        m = re.match('\s*@([A-Za-z0-9_]*)\s+', to_parse)
+        if not m:
+            return to_parse
+
+        define_type = m.group(1)
+        to_parse = to_parse[len(m.group(0)):]
+
+        if define_type == 'import':
+            current = {}
+            to_parse = parse_url(current, to_parse)
+            to_parse = strip_comments(open(current['value']).read()) + to_parse
+
+    return to_parse
--- parser/parse_file.py
+++ parser/parse_file.py
@@ -1,3 +1,4 @@
+from .parse_defines import parse_defines
 from .parse_selectors import parse_selectors
 from .parse_properties import parse_properties
 from .strip_comments import strip_comments
@@ -6,11 +7,15 @@ import re
 def parse_file(stat, file):
     content = open(file).read()
     stat['statements'] = []
+    stat['defines'] = {}
 
     to_parse = strip_comments(content)
 
 # read statements until there's only whitespace left
     while not re.match('\s*$', to_parse):
+        print(to_parse)
+        to_parse = parse_defines(stat, to_parse)
+
         selectors = []
         to_parse = parse_selectors(selectors, to_parse)
 
