3ebf0b3ccbaa1d30da38798ccef73d7d69d592c8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-04 07:33:23 +0100

    python-reimpl: compile eval statement

---
+++ compiler/compile_eval.py
@@ -0,0 +1,51 @@
+import pg
+import db
+import re
+eval_param = ', object, current, render_context'
+
+def valid_func_name(func):
+    if re.match('[a-zA-Z_0-9]+$', func):
+        return func
+
+    else:
+        raise Exception('Illegal eval function name: ' + func)
+
+def compile_eval(value):
+    global eval_param
+
+    if type(value) == str:
+        if value[0:2] == 'v:':
+            return pg.format(value[2:])
+        elif value[0:2] == 'f:':
+            return 'eval_' + valid_func_name(value[2:]) + '(\'{}\'' + eval_param + ')'
+        else:
+            raise Exception('compiling eval: ' + repr(value))
+
+    if len(value) == 0:
+        return ''
+
+    if not value[0][0:2] in ('f:', 'o:'):
+        return compile_eval(value[0])
+
+    param = [ compile_eval(i) for i in value[1:] ]
+
+    if value[0][0:2] == 'o:':
+        conn = db.connection()
+        res = conn.prepare('select * from eval_operators where op = $1')
+        result = res(value[0][2:])
+        if not len(result):
+            raise Exception('compiling eval: unknown operator ' + result[0][2:])
+        func = result[0]['func']
+
+    elif value[0][0:2] == 'f:':
+        func = value[0][2:]
+
+    if func == 'cond':
+        ret = '(CASE WHEN eval_boolean(Array[' + param[0] + ']' + eval_param + ') = \'true\' THEN ' + param[1]
+        if len(param) > 2:
+            ret += ' ELSE ' + param[2]
+        ret += ' END)'
+        return ret
+
+    else:
+        return 'eval_' + valid_func_name(func) + '(Array[' + ', '.join(param) + ']::text[]' + eval_param + ')'
--- compiler/compile_properties.py
+++ compiler/compile_properties.py
@@ -1,18 +1,25 @@
 import pg
+from .compile_eval import compile_eval
 
 def compile_properties(statement, stat):
     ret = ''
     to_set = { 'prop': {}, 'tags': {} }
 
     for prop in statement['properties']:
+        if not prop['key'] in stat['properties_values']:
+            stat['properties_values'][prop['key']] = set()
+
         if prop['assignment_type'] == 'P':
             if prop['value_type'] == 'eval':
-                pass
+                ret += print_props_and_tags(statement['current_pseudo_element'], to_set)
+                ret += 'current.styles[' + statement['current_pseudo_element'] + '] = ' +\
+                    'current.styles[' + statement['current_pseudo_element'] + '] || ' +\
+                    'hstore(' + pg.format(prop['key']) + ', ' +\
+                    compile_eval(prop['value']) + ');\n'
+
+                stat['properties_values'][prop['key']].add(True)
 
             else:
-                if not prop['key'] in stat['properties_values']:
-                    stat['properties_values'][prop['key']] = set()
-
                 if prop['value_type'] == 'value' and \
                     'type' in stat['defines'] and \
                     prop['key'] in stat['defines']['type']:
@@ -32,7 +39,11 @@ def compile_properties(statement, stat):
 
         elif prop['assignment_type'] == 'T':
             if prop['value_type'] == 'eval':
-                pass
+                ret += print_props_and_tags(statement['current_pseudo_element'], to_set)
+                ret += 'current.tags = ' +\
+                    'current.tags || ' +\
+                    'hstore(' + pg.format(prop['key']) + ', ' +\
+                    compile_eval(prop['value']) + ');\n'
 
             else:
                 to_set['tags'][prop['key']] = prop['value']
