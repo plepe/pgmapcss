1f3509d6ac0c380b95bc21c4c90ca9e1ca9c7d91
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-04 18:59:23 +0100

    python-reimpl: implement match function

--- compiler/compile_style.py
+++ compiler/compile_style.py
@@ -1,5 +1,6 @@
 from .compile_function_check import compile_function_check
 from .compile_function_get_where import compile_function_get_where
+from .compile_function_match import compile_function_match
 
 def compile_style(id, stat):
     ret = {}
@@ -10,5 +11,6 @@ def compile_style(id, stat):
     ))
     ret['function_check'] = compile_function_check(id, stat)
     ret['function_get_where'] = compile_function_get_where(id, stat)
+    ret['function_match'] = compile_function_match(id, stat)
 
     return ret
