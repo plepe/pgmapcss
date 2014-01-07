b76816fd5bbf1b19658247fb45ee3ae9df4fe04c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-07 10:15:32 +0100

    python-reimpl: parse value: rgb()

--- parser/parse_value.py
+++ parser/parse_value.py
@@ -54,9 +54,35 @@ def parse_value(current, to_parse):
         return
 
     # rgb(R, G, B)
-    elif to_parse.match('rgb\s*\((.*)\)\s*;'):
-        # TODO
-        current['value'] = to_parse.match_group(1)
+    elif to_parse.match('rgb\s*\(([0-9\.,%\s]+)\)\s*;'):
+        values = to_parse.match_group(1).split(',')
+        if len(values) != 3:
+            raise ParseError(to_parse, 'Error parsing rgb() value: "' + v + '"')
+
+        for i, v in enumerate(values):
+            if re.match('\s*[0-9]+\s*$', v): # integer: 0..255
+                values[i] = int(v)
+                if values[i] < 0 or values[i] > 255:
+                    raise ParseError(to_parse, 'Error parsing rgb() value, value {} out of range (0..255)'.format(v))
+
+            elif re.match('\s*([0-9]+\.[0-9]+)\s*$', v): # float: 0.0..1.0
+                values[i] = float(v)
+                if values[i] < 0.0 or values[i] > 1.0:
+                    raise ParseError(to_parse, 'Error parsing rgb() value, value {} out of range (0.0..1.0)'.format(v))
+                values[i] = round(v * 255)
+
+            elif re.match('\s*([0-9]+(\.[0-9]+)?)%\s*$', v): # percentage: 0%..100%
+                m = re.match('\s*([0-9]+(\.[0-9]+)?)%\s*$', v)
+                values[i] = float(m.group(1))
+                print(values[i])
+                if values[i] < 0.0 or values[i] > 100.0:
+                    raise ParseError(to_parse, 'Error parsing rgb() value, value {} out of range (0%..100%)'.format(v))
+                values[i] = round(values[i] * 2.55)
+
+            else:
+                raise ParseError(to_parse, 'Error parsing rgb() value, number format not detected: {}'.format(v))
+
+        current['value'] = '#' + ''.join([ '%02x' % v for v in values ])
         current['value_type'] = 'rgb'
         return
 
