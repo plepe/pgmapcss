57aa8d273f58c94f2b8cb557405ce2f56878549c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-11 09:41:36 +0100

    Versioning: update to 0.6-dev

--- pgmapcss/version.py
+++ pgmapcss/version.py
@@ -1,10 +1,12 @@
 __all__ = 'VERSION', 'VERSION_INFO'
 
 #: (:class:`tuple`) The version tuple e.g. ``(0, 9, 2)``.
-VERSION_INFO = (0, 5, 0)
+VERSION_INFO = (0, 6, 'dev')
 
 #: (:class:`basestring`) The version string e.g. ``'0.9.2'``.
 if len(VERSION_INFO) == 4:
     VERSION = '%d.%d.%d-%s' % VERSION_INFO
+elif type(VERSION_INFO[2]) == str:
+    VERSION = '%d.%d-%s' % VERSION_INFO
 else:
     VERSION = '%d.%d.%d' % VERSION_INFO
