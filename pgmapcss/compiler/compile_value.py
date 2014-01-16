4a1129817596fa5109e24eb65adbe1d980101a0a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-16 20:06:09 +0100

    Compile Properties / Values: two bugfixes
    - change return value for a tag which can be added to the 'to_set' array to
      False (as None is a valid value)
    - extra handling for value None, produced slightly incorrect value for
      'tag_value' tags: current.tags->null
      this evaluates to null, but is quite redundant

--- pgmapcss/compiler/compile_value.py
+++ pgmapcss/compiler/compile_value.py
@@ -2,19 +2,22 @@ import pgmapcss.db as db
 from .compile_eval import compile_eval
 
 def compile_value(prop, stat):
-    """returns two values, first a static value which can be put into the 'to_set' dictts, or None if the value is dynamic. The second value is the compiled version."""
+    """returns two values, first a static value which can be put into the 'to_set' dictts, or False if the value is dynamic. The second value is the compiled version."""
     if prop['value_type'] == 'eval':
-        return None, compile_eval(prop['value'])
+        return False, compile_eval(prop['value'])
 
     elif 'unit' in prop and prop['unit'] in ('m', 'u'):
-        return None, compile_eval([ 'f:metric', 'v:' + prop['value'] + prop['unit'] ])
+        return False, compile_eval([ 'f:metric', 'v:' + prop['value'] + prop['unit'] ])
+
+    elif prop['value'] == None:
+        return None, 'null'
 
     elif prop['value_type'] == 'value' and \
         prop['assignment_type'] == 'P' and \
         'type' in stat['defines'] and \
         prop['key'] in stat['defines']['type'] and \
         stat['defines']['type'][prop['key']]['value'] == "tag_value":
-        return None, '(current.tags)->' + db.format(prop['value'])
+        return False, '(current.tags)->' + db.format(prop['value'])
 
     else:
         return prop['value'], db.format(prop['value'])
