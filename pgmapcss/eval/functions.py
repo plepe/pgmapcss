fadb072bcccb08d31ccd2f9960fd8a2a81d60951
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-19 03:48:06 +0100

    pgmapcss.eval: function for automated tests

--- pgmapcss/eval/functions.py
+++ pgmapcss/eval/functions.py
@@ -37,3 +37,49 @@ class Functions:
             f['src'] = src
 
         self.eval_functions[func] = f
+
+    def test(self, func, conf):
+        print('* Testing %s' % func)
+
+        if 'src' not in conf:
+            return
+
+        import re
+        import pgmapcss.db as db
+        rows = conf['src'].split('\n')
+
+        ret = '''
+create or replace function __eval_test__(param text[]) returns text
+as $body$
+import re
+current = { 'object': { 'id': 'n123', 'tags': { 'amenity': 'restaurant', 'name': 'Foobar', 'cuisine': 'pizza;kebab;noodles' }}, 'pseudo_element': 'default', 'pseudo_elements': ['default', 'test'], 'tags': { 'amenity': 'restaurant', 'name': 'Foobar', 'cuisine': 'pizza;kebab;noodles' }, 'properties': { 'default': { 'width': '2', 'color': '#ff0000' }, 'test': { 'fill-color': '#00ff00' } } }
+render_context = {'bbox': '010300002031BF0D000100000005000000DBF1839BB5DC3B41E708549B2B705741DBF1839BB5DC3B41118E9739B171574182069214CCE23B41118E9739B171574182069214CCE23B41E708549B2B705741DBF1839BB5DC3B41E708549B2B705741', 'scale_denominator': 8536.77}
+'''
+        ret += self.print()
+        ret += 'return eval_' + func + '(param)'
+        ret += "$body$ language 'plpython3u' immutable;"
+        conn = db.connection()
+        conn.execute(ret)
+
+        param_in = None
+        for r in rows:
+            m = re.match('# IN (.*)$', r)
+            if m:
+                param_in = eval(m.group(1))
+
+            m = re.match('# OUT (.*)$', r)
+            if m:
+                return_out = eval(m.group(1))
+
+                r = conn.prepare('select __eval_test__($1)');
+                res = r(param_in)
+
+                print(' IN  %s' % repr(param_in))
+                print(' EXP %s' % repr(return_out))
+                print(' OUT %s' % repr(res[0][0]))
+
+                if repr(return_out) != repr(res[0][0]):
+                    raise Exception("eval-test failed!")
+
+    def test_all(self):
+        [ self.test(func, conf) for func, conf in self.eval_functions.items() ]
