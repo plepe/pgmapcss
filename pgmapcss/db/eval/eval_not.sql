819e1a7d4ff0401c2a2626ced0ffa6367ce7e2b3
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-19 03:06:06 +0100

    eval-function: Bugfix: 'not' - wrong function name

--- pgmapcss/db/eval/eval_not.sql
+++ pgmapcss/db/eval/eval_not.sql
@@ -1,4 +1,4 @@
-create or replace function eval_and(param text[],
+create or replace function eval_not(param text[],
   object pgmapcss_object, current pgmapcss_current, render_context pgmapcss_render_context)
 returns text
 as $$
