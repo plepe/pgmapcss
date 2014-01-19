20e794f47ff954e3e71f89cd6232368a6542e4fe
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-01-19 10:12:04 +0100

    compile function check(): re-activate stuff from form plpgsql mode
    - @default_other, @values, @postprocess, @depend_property
    - order results by object-z-index

--- pgmapcss/compiler/compile_function_check.py
+++ pgmapcss/compiler/compile_function_check.py
@@ -1,19 +1,16 @@
 from .compile_statement import compile_statement
 from .compile_eval import compile_eval
 from .stat import *
-import pgmapcss.db as db
 import pgmapcss.eval
 
-def print_checks(prop, stat, main_prop=None):
+def print_checks(prop, stat, main_prop=None, indent=''):
     ret = ''
 
     # @default_other
     if 'default_other' in stat['defines'] and prop in stat['defines']['default_other']:
         other = stat['defines']['default_other'][prop]['value']
-        ret += 'if (current.styles[r.i]->' + db.format(prop) + ') is null ' +\
-            'then current.styles[r.i] := current.styles[r.i] || hstore(' +\
-            db.format(prop) + ', current.styles[r.i]->' +\
-            db.format(other) + '); end if;\n'
+        ret += indent + 'if not ' + repr(prop) + " in current['properties'][pseudo_element] or current['properties'][pseudo_element][" + repr(prop) + "] is None:\n"
+        ret += indent + "    current['properties'][pseudo_element][" + repr(prop) + "] = current['properties'][pseudo_element][" + repr(other) + "] if " + repr(other) + " in current['properties'][pseudo_element] else None\n"
 
     # @values
     if 'values' in stat['defines'] and prop in stat['defines']['values']:
@@ -24,12 +21,8 @@ def print_checks(prop, stat, main_prop=None):
         # resulting value and - if not allowed - replace by the first
         # allowed value
         if len([ v for v in used_values if not v in values ]):
-            ret += 'if not (current.styles[r.i]->' +\
-                db.format(prop) + ') = any(' +\
-                db.format(values) + ') then ' +\
-                'current.styles[r.i] := current.styles[r.i] || hstore(' +\
-                db.format(prop) + ', ' +\
-                db.format(values[0]) + '); end if;\n';
+            ret += indent + 'if ' + repr(prop) + " not in current['properties'][pseudo_element] or current['properties'][pseudo_element][" + repr(prop) + "] not in " + repr(values) + ":\n"
+            ret += indent + "    current['properties'][pseudo_element][" + repr(prop) + "] = " + repr(values[0]) + '\n'
 
     return ret
 
@@ -47,11 +40,11 @@ create or replace function {style_id}_check(
 import pghstore
 import re
 # eval-functions
-def to_float(v):
+def to_float(v, default=None):
     try:
         return float(v)
     except ValueError:
-        return None
+        return default
 '''.format(**replacement)
 
     ret += pgmapcss.eval.load().print()
@@ -90,9 +83,41 @@ ret = {{
     'combine_id': None,
 }}
 
-for pseudo_element in {pseudo_elements}:
+# iterate over all pseudo-elements, sorted by 'object-z-index' if available
+for pseudo_element in sorted({pseudo_elements}, key=lambda s: to_float(current['properties'][s]['object-z-index'], 0.0) if 'object-z-index' in current['properties'][s] else 0):
     if current['has_pseudo_element'][pseudo_element]:
+        current['pseudo_element'] = pseudo_element # for eval functions
         ret['pseudo_element'] = pseudo_element
+'''.format(**replacement)
+
+    # handle @values, @default_other for all properties
+    done_prop = []
+    indent = '        '
+    # start with props from @depend_property
+    for main_prop, props in stat['defines']['depend_property'].items():
+        props = props['value'].split(';')
+        r = ''
+
+        r += print_checks(main_prop, stat, indent=indent + '    ')
+        done_prop.append(main_prop)
+
+        for prop in props:
+            r += print_checks(prop, stat, main_prop=main_prop, indent=indent + '    ')
+            done_prop.append(prop)
+
+        if r != '':
+            ret += indent + 'if ' + repr(main_prop) + " in current['properties'][pseudo_element]:\n"
+            ret += r
+
+    for prop in [ prop for prop in stat_properties(stat) if not prop in done_prop ]:
+        ret += print_checks(prop, stat, indent)
+
+    # postprocess requested properties (see @postprocess)
+    for k, v in stat['defines']['postprocess'].items():
+        ret += indent + "current['properties'][pseudo_element][" + repr(k) +\
+               "] = " + compile_eval(v['value']) + '\n'
+
+    ret += '''\
         # set geo as return value AND remove key from properties
         ret['geo'] = current['properties'][pseudo_element].pop('geo');
         ret['properties'] = pghstore.dumps(current['properties'][pseudo_element])
