7fb6ff6b4bb45a5ca881a1054d96ffbf892e972a
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-02-17 05:18:20 +0100

    Exception 'CompileError'

---
+++ pgmapcss/compiler/CompileError.py
@@ -0,0 +1,6 @@
+class CompileError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
--- pgmapcss/compiler/__init__.py
+++ pgmapcss/compiler/__init__.py
@@ -1,2 +1,3 @@
 from .compile_style import compile_style
 from .stat import *
+from .CompileError import CompileError
