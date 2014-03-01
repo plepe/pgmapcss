4b0e3279f38bac7f4bcc14b5923ed649daeacc1d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-03-01 03:01:23 +0100

    Eval::min(), max(): Bugfix, really convert values via metric()

--- pgmapcss/eval/eval_max.py
+++ pgmapcss/eval/eval_max.py
@@ -4,8 +4,8 @@ def eval_max(param):
     if len(param) == 1:
         param = param[0].split(';')
 
-    values = [ eval_metric(v) for v in param ]
-    values = [ float(v) for v in param if v != '' ]
+    values = [ eval_metric([v]) for v in param ]
+    values = [ float(v) for v in values if v != '' ]
 
     while '' in values:
         values.remove('')
@@ -21,6 +21,8 @@ def eval_max(param):
 # OUT '1'
 # IN ['1;4;5']
 # OUT '5'
+# IN ['1px;1000u']
+# OUT '418.35829844643484'
 # IN []
 # OUT ''
 # IN ['1;;5']
--- pgmapcss/eval/eval_min.py
+++ pgmapcss/eval/eval_min.py
@@ -4,8 +4,8 @@ def eval_min(param):
     if len(param) == 1:
         param = param[0].split(';')
 
-    values = [ eval_metric(v) for v in param ]
-    values = [ float(v) for v in param if v != '' ]
+    values = [ eval_metric([v]) for v in param ]
+    values = [ float(v) for v in values if v != '' ]
 
     while '' in values:
         values.remove('')
@@ -20,6 +20,8 @@ def eval_min(param):
 # IN ['1.0', '']
 # OUT '1'
 # IN ['1;4;5']
+# OUT '1'
+# IN ['1px;1000m']
 # OUT '1'
 # IN []
 # OUT ''
