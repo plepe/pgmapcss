5e619b4654d76159eb7a89404c7d0d1d9de6672f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-03-12 20:33:20 +0100

    Eval::cond: add eval tests

--- pgmapcss/eval/eval_cond.py
+++ pgmapcss/eval/eval_cond.py
@@ -7,3 +7,22 @@ class config_eval_cond(config_base):
             ret += " else ''"
         ret += ')'
         return ret
+
+# IN [ 'true', 'A' ]
+# OUT 'A'
+# IN [ 'true', 'A', 'B' ]
+# OUT 'A'
+# IN [ 'false', 'A' ]
+# OUT ''
+# IN [ 'false', 'A', 'B' ]
+# OUT 'B'
+# IN [ '0', 'A', 'B' ]
+# OUT 'B'
+# IN [ 'no', 'A', 'B' ]
+# OUT 'B'
+# IN [ '', 'A', 'B' ]
+# OUT 'B'
+# IN [ 'foo', 'A', 'B' ]
+# OUT 'A'
+# IN [ 'bar', 'A', 'B' ]
+# OUT 'A'
