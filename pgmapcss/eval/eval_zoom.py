83a5da7c78afbf028f69ac2da46317b02e7f53de
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-03-17 07:45:10 +0100

    Eval::zoom(): only return zoom levels of current statement

--- pgmapcss/eval/eval_zoom.py
+++ pgmapcss/eval/eval_zoom.py
@@ -2,7 +2,18 @@ class config_eval_zoom(config_base):
     mutable = 3
 
     def possible_values(self, param_values, prop, stat):
-        return { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20' }
+        import math
+
+        min_zoom = 0
+        max_zoom = 20
+
+        if 'statement' in prop and 'selector' in prop['statement']:
+            if 'min_scale' in prop['statement']['selector'] and prop['statement']['selector']['min_scale'] > 0:
+                max_zoom = int(math.ceil(math.log(3.93216e+08 / prop['statement']['selector']['min_scale'], 2)))
+            if 'max_scale' in prop['statement']['selector'] and prop['statement']['selector']['max_scale'] is not None:
+                min_zoom = int(math.ceil(math.log(3.93216e+08 / (prop['statement']['selector']['max_scale'] - 0.1), 2)))
+
+        return { str(i) for i in range(min_zoom, max_zoom + 1) }
 
 def eval_zoom(param):
   import math
