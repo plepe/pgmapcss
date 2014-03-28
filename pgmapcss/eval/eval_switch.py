bf712472bb8270634bea8a7fc59ee40ad1cc610d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-03-28 20:18:41 +0100

    Eval::switch(): possible values

--- pgmapcss/eval/eval_switch.py
+++ pgmapcss/eval/eval_switch.py
@@ -1,6 +1,20 @@
 class config_eval_switch(config_base):
     mutable = 3
 
+    def possible_values(self, param_values, prop, stat):
+        if param_values[0] is not True:
+            return config_base.possible_values(self, param_values, prop, stat)
+
+        ret = {}
+
+        for i in range(1, len(param) - 1, 2):
+            ret.add(param[i + 1])
+
+        if len(param) % 2 == 0:
+            ret.add(param[-1])
+
+        return ( ret, 3 )
+
 def eval_switch(param):
     if len(param) == 0:
         return ''
