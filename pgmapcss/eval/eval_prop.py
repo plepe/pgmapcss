7545b781eef78a35f7ec92c2abb73df0774113be
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-03-28 14:04:33 +0100

    Eval::prop(): possible values (all previously set values for specified pseudo element)

--- pgmapcss/eval/eval_prop.py
+++ pgmapcss/eval/eval_prop.py
@@ -1,3 +1,22 @@
+class config_eval_prop(config_base):
+    def possible_values(self, param_values, prop, stat):
+        import pgmapcss.compiler.stat
+
+        if len(param_values) > 1:
+            pseudo_element = param_values[1]
+        elif not 'statement' in prop or \
+             not 'selector' in prop['statement'] or \
+             not 'pseudo_element' in prop['statement']['selector']:
+            return ( True, 0 )
+        else:
+            pseudo_element = prop['statement']['selector']['pseudo_element']
+
+        if not 'id' in prop:
+            return ( True, 0 )
+
+        values = pgmapcss.compiler.stat.stat_property_values(param_values[0], stat, pseudo_element=pseudo_element, max_prop_id=prop['id'] - 1)
+        return ( values, 0 )
+
 def eval_prop(param):
     if len(param) == 0:
         return ''
