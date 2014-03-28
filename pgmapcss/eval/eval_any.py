ad3539f14e2a8d69d1b50cc5697ad42b8488c2e4
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-03-28 20:58:06 +0100

    Eval::any(): possible values

--- pgmapcss/eval/eval_any.py
+++ pgmapcss/eval/eval_any.py
@@ -1,6 +1,18 @@
 class config_eval_any(config_base):
     mutable = 3
 
+    def possible_values(self, param_values, prop, stat):
+        ret = set()
+
+        for p in param_values:
+            if p is not None and p != '':
+                ret.add(p)
+
+                if p is not True:
+                    return ( ret, 3 )
+
+        return ( ret, 3 )
+
 def eval_any(param):
     for p in param:
         if p is not None and p != '':
