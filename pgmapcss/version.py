69c18335e04c939d9da7074d1946a23616a30cad
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-05-22 07:17:37 +0200

    Change to version 0.8-dev

--- pgmapcss/version.py
+++ pgmapcss/version.py
@@ -1,7 +1,7 @@
 __all__ = 'VERSION', 'VERSION_INFO'
 
 #: (:class:`tuple`) The version tuple e.g. ``(0, 9, 2)``.
-VERSION_INFO = (0, 7, 0)
+VERSION_INFO = (0, 8, 'dev')
 
 #: (:class:`basestring`) The version string e.g. ``'0.9.2'``.
 if len(VERSION_INFO) == 4:
