f6f440ba74df2acc3ded7efb0b574b7323d45eb1
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-06-14 20:57:16 +0200

    Parser: Selector tests may appear in arbitrary order
    closes #13

--- pgmapcss/parser/parse_selectors.py
+++ pgmapcss/parser/parse_selectors.py
@@ -20,49 +20,54 @@ def parse_selector_part(to_parse, object_class_selector='\*|[a-z_]+'):
     else:
         raise ParseError(to_parse, 'Can\'t parse object class')
 
+    last_pos = None
+
+    while last_pos != to_parse.pos():
+        last_pos = to_parse.pos()
+
 # parse classes
-    while to_parse.match('\.([a-zA-Z0-9_]+)'):
-        if 'classes' in current:
-            pass
-        else:
-            current['classes'] = []
+        if to_parse.match('\.([a-zA-Z0-9_]+)'):
+            if 'classes' in current:
+                pass
+            else:
+                current['classes'] = []
 
-        current['classes'].append(to_parse.match_group(1))
+            current['classes'].append(to_parse.match_group(1))
 
-        current['conditions'].append({ 'op': 'has_tag', 'key': '.' + to_parse.match_group(1) })
+            current['conditions'].append({ 'op': 'has_tag', 'key': '.' + to_parse.match_group(1) })
 
 # parse zoom level
-    if to_parse.match('\|z([0-9]*)(-?)([0-9]*)'):
-        if to_parse.match_group(1):
-            current['max_scale'] = max_scale_denominator / 2 ** (int(to_parse.match_group(1)) - 1)
+        if to_parse.match('\|z([0-9]*)(-?)([0-9]*)'):
+            if to_parse.match_group(1):
+                current['max_scale'] = max_scale_denominator / 2 ** (int(to_parse.match_group(1)) - 1)
 
-        if to_parse.match_group(1) and not to_parse.match_group(2):
-            current['min_scale'] = max_scale_denominator / 2 ** (int(to_parse.match_group(1)))
+            if to_parse.match_group(1) and not to_parse.match_group(2):
+                current['min_scale'] = max_scale_denominator / 2 ** (int(to_parse.match_group(1)))
 
-        if to_parse.match_group(3):
-            current['min_scale'] = max_scale_denominator / 2 ** (int(to_parse.match_group(3)))
+            if to_parse.match_group(3):
+                current['min_scale'] = max_scale_denominator / 2 ** (int(to_parse.match_group(3)))
 
 # parse conditions - TODO
-    while to_parse.match('\['):
-        result = parse_condition(to_parse)
-        if result:
-            current['conditions'].append(result)
+        if to_parse.match('\['):
+            result = parse_condition(to_parse)
+            if result:
+                current['conditions'].append(result)
 
 # parse pseudo classes
-    while to_parse.match(':([a-zA-Z0-9_]+)'):
-        if 'pseudo_classes' in current:
-            pass
-        else:
-            current['pseudo_classes'] = []
+        if to_parse.match(':([a-zA-Z0-9_]+)'):
+            if 'pseudo_classes' in current:
+                pass
+            else:
+                current['pseudo_classes'] = []
 
-        current['pseudo_classes'].append(to_parse.match_group(1))
+            current['pseudo_classes'].append(to_parse.match_group(1))
 
 # parse pseudo element
-    while to_parse.match('::(\(?)([a-zA-Z0-9_\*\-]+)(\)?)'):
-        if to_parse.match_group(1) and to_parse.match_group(3):
-            current['create_pseudo_element'] = False
+        if to_parse.match('::(\(?)([a-zA-Z0-9_\*\-]+)(\)?)'):
+            if to_parse.match_group(1) and to_parse.match_group(3):
+                current['create_pseudo_element'] = False
 
-        current['pseudo_element'] = to_parse.match_group(2)
+            current['pseudo_element'] = to_parse.match_group(2)
 
     return current
 
