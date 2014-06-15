3d295b9302ca160a4a43d821aaa7ec8bd14ff1c7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-06-15 19:12:20 +0200

    Support pseudo class ':closed'

--- pgmapcss/compiler/compile_pseudo_class_condition.py
+++ pgmapcss/compiler/compile_pseudo_class_condition.py
@@ -1,6 +1,11 @@
+from .compile_eval import compile_eval
+
 def compile_pseudo_class_condition(condition, stat):
     if condition['key'] in ('active', 'hover'):
         return 'False'
+
+    elif condition['key'] == 'closed':
+        return compile_eval('f:is_closed', condition, stat) + " == 'true'"
 
     else:
         print('unknown/unsupported pseudo class: {key}'.format(**condition))
