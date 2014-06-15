1740f6ac00b2b9cb9dcd04159ec4a1a62b7dcefa
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-06-15 19:12:06 +0200

    Eval: new eval function 'is_closed'

---
+++ pgmapcss/eval/eval_is_closed.py
@@ -0,0 +1,14 @@
+class config_is_closed(config_base):
+    mutable = 1
+
+def eval_is_closed(param):
+    if len(param) > 0:
+        geo = param[0]
+
+    else:
+        geo = current['properties'][current['pseudo_element']].get('geo')
+
+    plan = plpy.prepare('select ST_GeometryType($1) in (\'ST_Polygon\', \'ST_MultiPolygon\') or (ST_GeometryType($1) in (\'ST_Line\') and ST_Line_Interpolate_Point($1, 0.0) = ST_Line_Interpolate_Point($1, 1.0)) as r', ['geometry'])
+    res = plpy.execute(plan, [ geo ])
+
+    return 'true' if res[0]['r'] else 'false'
