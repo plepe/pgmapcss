825cb4fd66635363053159f913ee241a46d63126
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-06-21 07:26:37 +0200

    Type dashes: improve check; use eval function to_dashes()

---
+++ pgmapcss/eval/eval_to_dashes.py
@@ -0,0 +1,37 @@
+class config_eval_to_dashes(config_base):
+    pass
+
+def eval_to_dashes(params):
+    if len(params) == 0 or params[0] == '':
+        return 'none'
+
+    ret = params[0]
+
+    if ret == 'none':
+        return ret
+
+    ret = str.replace(ret, ';', ',')
+    ret = str.replace(ret, ' ', '')
+
+    if len([
+        t
+        for t in ret.split(',')
+        if not re.match('[0-9]+$', t)
+    ]):
+        plpy.notice("invalid dashes value '{}'".format(params[0]))
+        return 'none'
+
+    return ret
+
+# IN ['2,4']
+# OUT '2,4'
+# IN ['2; 4']
+# OUT '2,4'
+# IN ['20;40']
+# OUT '20,40'
+# IN ['20;;40']
+# OUT 'none'
+# IN ['none']
+# OUT 'none'
+# IN ['foo,5']
+# OUT 'none'
--- pgmapcss/types/dashes.py
+++ pgmapcss/types/dashes.py
@@ -1,12 +1,27 @@
 from .default import default
+from ..compiler.compile_eval import compile_eval
+import re
 
 class dashes(default):
     def compile_check(self, value):
-        return 'str.replace(str.replace(' + value + ", ';', ','), ' ', '')"
+        return 'eval_to_dashes([' + value + '])'
 
-    def __parse(self, ret):
+    def __parse(self, param):
+        if param == 'none':
+            return param
+
+        ret = param
         ret = str.replace(ret, ';', ',')
         ret = str.replace(ret, ' ', '')
+
+        if len([
+            t
+            for t in ret.split(',')
+            if not re.match('[0-9]+$', t)
+        ]):
+            print("invalid dashes value '{}'".format(param))
+            return 'none'
+
         return ret
 
     def compile(self, prop):
