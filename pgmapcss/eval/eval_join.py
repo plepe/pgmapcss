7a18022e0bab7d519f30e8c3efd0e11f3e3f5481
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-07-03 07:38:08 +0200

    Eval: split(), join(): change order of parameters

--- pgmapcss/eval/eval_join.py
+++ pgmapcss/eval/eval_join.py
@@ -5,10 +5,10 @@ def eval_join(param):
     if len(param) < 2:
         return ''
 
-    l = param[0].split(';')
+    l = param[1].split(';')
 
-    return param[1].join(l)
+    return param[0].join(l)
 
 # TESTS
-# IN ['restaurant;bar', ', ']
+# IN [', ', 'restaurant;bar']
 # OUT 'restaurant, bar'
