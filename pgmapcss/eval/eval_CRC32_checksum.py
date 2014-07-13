84c714b0641a08c964c2f5e6c429c36fc1f7341d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-07-13 21:05:22 +0200

    Eval functions: encode functions
    - URL_encode()
    - XML_encode()
    - CRC32_checksum()

---
+++ pgmapcss/eval/eval_CRC32_checksum.py
@@ -0,0 +1,14 @@
+class config_eval_CRC32_checksum(config_base):
+    mutable = 3
+
+def eval_CRC32_checksum(param):
+    if len(param) == 0:
+        return ''
+
+    import binascii
+
+    return str(binascii.crc32(param[0].encode()))
+
+# TESTS
+# IN [ 'foobar' ]
+# OUT '2666930069'
---
+++ pgmapcss/eval/eval_URL_encode.py
@@ -0,0 +1,15 @@
+class config_eval_URL_encode(config_base):
+    mutable = 3
+
+def eval_URL_encode(param):
+    if len(param) == 0:
+        return ''
+
+    import urllib.parse
+    return urllib.parse.quote(param[0])
+
+# TESTS
+# IN [ 'test/' ]
+# OUT 'test/'
+# IN [ 'El Niño' ]
+# OUT 'El%20Ni%C3%B1o'
