20fdaf56626e2d2622a3db6d0c7c08f2311615f1
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-07-14 10:25:03 +0200

    Compiler pseudo classes: support :connection and :unconnected

--- pgmapcss/compiler/compile_pseudo_class_condition.py
+++ pgmapcss/compiler/compile_pseudo_class_condition.py
@@ -7,6 +7,12 @@ def compile_pseudo_class_condition(condition, stat):
     elif condition['key'] == 'closed':
         return [compile_eval('f:is_closed', condition, stat) + " == 'true'"]
 
+    elif condition['key'] == 'connection':
+        return ["len(list(objects_member_of(current['object']['id'], 'way', None))) > 1"]
+
+    elif condition['key'] == 'unconnected':
+        return ["len(list(objects_member_of(current['object']['id'], 'way', None))) == 0"]
+
     else:
         print('unknown/unsupported pseudo class: {key}'.format(**condition))
         return ['False']
