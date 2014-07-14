1573cb29a39a592ccb2508fbd38fa94a836c8239
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-07-14 15:18:14 +0200

    Compile pseudo class: :tagged

--- pgmapcss/compiler/compile_pseudo_class_condition.py
+++ pgmapcss/compiler/compile_pseudo_class_condition.py
@@ -13,6 +13,9 @@ def compile_pseudo_class_condition(condition, stat):
     elif condition['key'] == 'unconnected':
         return ["len(list(objects_member_of(current['object']['id'], 'way', None))) == 0"]
 
+    elif condition['key'] == 'tagged':
+        return ["len({ k: v for k, v in current['tags'].items() if not re.match('(source.*|note|comment|converted_by|created_by|watch.*|fixme|FIXME|description|attribution)$') }) != 0"]
+
     else:
         print('unknown/unsupported pseudo class: {key}'.format(**condition))
         return ['False']
