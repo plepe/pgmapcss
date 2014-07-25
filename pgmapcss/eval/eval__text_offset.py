a13c1c469a9f26bddb89bd3b901a3caf21e47eb1
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-07-25 09:52:24 +0200

    New property: text-anchor-vertical (below, above)

--- pgmapcss/eval/eval__text_offset.py
+++ pgmapcss/eval/eval__text_offset.py
@@ -11,26 +11,34 @@ class config_eval__text_offset(config_base):
 # 5 ... symbol-size
 # 6 ... symbol-stroke-width
         import math
-        ret = { 0.0 } # default: no offset
+        values = set()
 
-        ret = ret.union(param_values[0]) # text-offset
+        values = values.union(param_values[0]) # text-offset
 
         # check if maki icon is used - use width values for height
         if len([c for c in param_values[2] if c is not True and not '.' in c]):
-            ret = ret.union({ math.ceil(float(w) / 2.0) + 1.0 for w in param_values[3] if w in ("12", "18", "24") }) # icon-width
+            values = values.union({ math.ceil(float(w) / 2.0) + 1.0 for w in param_values[3] if w in ("12", "18", "24") }) # icon-width
 
         # other icons -> read dimensions from global_data
         if 'global_data' in stat and 'icon-image' in stat['global_data']:
             for c in param_values[2]:
                 if c in stat['global_data']['icon-image']:
                     d = stat['global_data']['icon-image'][c]
-                    ret.add(math.ceil(float(d[1]) / 2.0) + 1.0)
+                    values.add(math.ceil(float(d[1]) / 2.0) + 1.0)
 
         # symbol-shape used
         if len(param_values[4]):
-            ret = ret.union({ math.ceil(float(w) / 2.0) + float(s) + 2.0 for w in param_values[5] for s in param_values[6] })
+            values = values.union({ math.ceil(float(w) / 2.0) + float(s) + 2.0 for w in param_values[5] for s in param_values[6] })
 
-        ret = { str(int(c)) for c in ret }
+
+        ret = { "0" }
+
+        # check text-vertical-anchor
+        if True in param_values[1] or 'below' in param_values[1]:
+            ret = ret.union({ str(int(c)) for c in values })
+
+        if True in param_values[1] or 'above' in param_values[1]:
+            ret = ret.union({ str(int(-c)) for c in values })
 
         return (ret, 0)
 
@@ -56,4 +64,13 @@ def eval__text_offset(param):
        'symbol-stroke-width' in prop and prop['symbol-stroke-width']:
         ret_symbol = math.ceil(float(prop['symbol-size']) / 2.0) + float(prop['symbol-stroke-width']) + 2.0
 
-    return float_to_str(max(ret_icon, ret_symbol))
+    min_max = max
+    if 'text-anchor-vertical' in prop:
+        if prop['text-anchor-vertical'] in (None, 'below'):
+            pass
+        elif prop['text-anchor-vertical'] in ('above'):
+            ret_icon = -ret_icon
+            ret_symbol = -ret_symbol
+            min_max = min
+
+    return float_to_str(min_max(ret_icon, ret_symbol))
