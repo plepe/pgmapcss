5729bbf3cb23e7bec5402dd2ab99305106e22b1c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-07-30 07:47:42 +0200

    Eval: min(), max() no longer accept input with units

--- pgmapcss/eval/eval_max.py
+++ pgmapcss/eval/eval_max.py
@@ -1,38 +1,5 @@
 class config_eval_max(config_base):
-    def mutable(self, param_values, stat):
-        import pgmapcss.eval
-
-        if len(param_values) == 1 and param_values[0] is not True:
-            param_values = param_values[0].split(';')
-
-        config_metric = pgmapcss.eval.eval_functions.list()['metric']
-        ret = [ config_metric.mutable([p], stat) for p in param_values ]
-        return min(ret)
-
-    def possible_values(self, param_values, prop, stat):
-        ret = set()
-
-        if len(param_values) == 0:
-            return ( '', self.mutable(param_values, stat) )
-
-        if len(param_values) == 1 and param_values[0] is True:
-            return ( True, 3 )
-
-        if len(param_values) == 1:
-            param_values = param_values[0].split(';')
-
-        if True in param_values:
-            ret.add(True)
-
-        values = [ eval_metric([v]) for v in param_values if v is not True ]
-        values = [ float(v) for v in values if v != '' ]
-
-        if len(values) == 0:
-            return ( '', 3 )
-
-        ret.add(float_to_str(max(values)))
-
-        return ( ret, self.mutable(param_values, stat ) )
+    mutable = 3
 
 def eval_max(param):
     if len(param) == 0:
@@ -40,7 +7,14 @@ def eval_max(param):
     if len(param) == 1:
         param = param[0].split(';')
 
-    values = [ eval_metric([v]) for v in param ]
+    values = []
+    for v in param:
+        try:
+            v = float(v)
+        except ValueError:
+            v = ''
+        values.append(v)
+
     values = [ float(v) for v in values if v != '' ]
 
     if len(values) == 0:
@@ -55,8 +29,6 @@ def eval_max(param):
 # OUT '1'
 # IN ['1;4;5']
 # OUT '5'
-# IN ['1px;1000u']
-# OUT '418.35829844643484'
 # IN []
 # OUT ''
 # IN ['1;;5']
