ebbf8d73555b17c14c6a621fea8a4efc77943143
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-07-30 14:58:47 +0200

    Eval: new function signum()

---
+++ pgmapcss/eval/eval_signum.py
@@ -0,0 +1,25 @@
+class config_eval_signum(config_base):
+    mutable = 3
+
+def eval_signum(param):
+    if len(param) == 0:
+        return ''
+
+    try:
+        v = float(param[0])
+    except ValueError:
+        return ''
+
+    if v < 0.0:
+        return '-1'
+    if v > 0.0:
+        return '1'
+    return '0'
+
+# TESTS
+# IN ['1']
+# OUT '1'
+# IN ['-5']
+# OUT '-1'
+# IN ['0']
+# OUT '0'
