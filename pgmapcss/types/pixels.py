90d289cce7c2fcac6b3295a99399b0f551f28cd8
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-08-12 18:52:12 +0200

    Type Pixels: calculate relative values in postprocess

--- pgmapcss/types/pixels.py
+++ pgmapcss/types/pixels.py
@@ -2,6 +2,12 @@ from .default import default
 from ..compiler.compile_eval import compile_eval
 import re
 
+def float_to_str(v, default=None):
+    r = repr(v)
+    if r[-2:] == '.0':
+        r = r[:-2]
+    return r
+
 class pixels(default):
     def _parse(self, value):
         if value is None:
@@ -18,23 +24,58 @@ class pixels(default):
         return None
 
 
+    def compile_postprocess(self):
+        return "if pseudo_element != 'default':\n" +\
+               "    current['properties'][pseudo_element][" + repr(self.key) + "] = " +\
+               compile_eval([ 'o:+', [ 'f:prop', 'v:' + self.key, 'v:default' ], [ 'f:prop', 'v:' + self.key ]], { 'key': self.key }, self.stat) + "\n" +\
+               "elif " + repr(self.key) + " in current['properties'][pseudo_element] and current['properties'][pseudo_element][" + repr(self.key) + "][0] == '+':\n" +\
+               "    current['properties'][pseudo_element][" + repr(self.key) + "] = " +\
+               "current['properties'][pseudo_element][" + repr(self.key) + "][1:]"
+
+    def stat_postprocess(self, values, pseudo_element=None):
+        ret = set()
+
+        default_values = self.stat.property_values(self.key, pseudo_element='default', include_illegal_values=False, postprocess=False)
+
+        if pseudo_element is None or pseudo_element == 'default':
+            ret = ret.union({
+                d[1:] if d[0] == '+' else d
+                for d in default_values
+            })
+
+        if pseudo_element:
+            pseudo_elements = { pseudo_element }
+        else:
+            pseudo_elements = self.stat['pseudo_elements'].copy()
+        if 'default' in pseudo_elements:
+            pseudo_elements.remove('default')
+
+        for p in pseudo_elements:
+            p_values = self.stat.property_values(self.key, pseudo_element=p, include_illegal_values=False, postprocess=False)
+            ret = ret.union({
+                float_to_str(float(d) + float(v)) if v[0] == '+' else v
+                for v in p_values
+                for d in default_values
+            })
+
+        return ret
+
     def compile(self, prop):
         v = self._parse(prop['value'])
 
         if v is None:
             return None
 
-        # relative value add/sub from value on default layer
+        prefix = ''
+
+        # relative value add from value on default layer; remember to add if value is evaluated from eval function
         if v[0][0] in ('+'):
-            if v[1] == 'px':
-                return self.compile_check(compile_eval([ 'o:+', [ 'f:prop', 'v:' + prop['key'], 'v:default' ], 'v:' + v[0] ], prop, self.stat))
-            else:
-                return self.compile_check(compile_eval([ 'o:+', [ 'f:prop', 'v:' + prop['key'], 'v:default' ], [ 'f:metric', 'v:' + v[0] + v[1] ] ], prop, self.stat))
+            prefix = "'+' + "
 
         if v[1] == 'px':
             return repr(v[0])
 
-        return self.compile_check(compile_eval([ 'f:metric', 'v:' + v[0] + v[1] ], prop, self.stat))
+        return prefix + self.compile_check(compile_eval([ 'f:metric', 'v:' + v[0] + v[1] ], prop, self.stat))
 
     def stat_value(self, prop):
         v = self._parse(prop['value'])
