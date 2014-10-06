f27a6d3d9bcbf430781c67a70858ece85bd15841
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-10-06 20:42:47 +0200

    Pseudo Classes: implement :lang()
    - closes #16

--- pgmapcss/compiler/compile_pseudo_class_condition.py
+++ pgmapcss/compiler/compile_pseudo_class_condition.py
@@ -1,11 +1,11 @@
 from .compile_eval import compile_eval
 
 def compile_pseudo_class_condition(condition, stat):
-    if condition['key'] not in ('active', 'hover', 'closed', 'connection', 'unconnected', 'tagged', 'righthandtraffic', 'lefthandtraffic'):
+    if condition['key'] not in ('active', 'hover', 'closed', 'connection', 'unconnected', 'tagged', 'righthandtraffic', 'lefthandtraffic', 'lang'):
         print('unknown/unsupported pseudo class: {key}'.format(**condition))
         return ['False']
 
-    if 'value' in condition:# and condition['key'] not in (,):
+    if 'value' in condition and condition['key'] not in ('lang',):
         print('pseudo class {key} does not accept a parameter -> ignoring'.format(**condition))
         return ['False']
 
@@ -29,3 +29,10 @@ def compile_pseudo_class_condition(condition, stat):
 
     elif condition['key'] == 'lefthandtraffic':
         return [compile_eval('f:is_left_hand_raffic', condition, stat) + " != 'false'"]
+
+    elif condition['key'] == 'lang':
+        if not 'value' in condition:
+            print('pseudo class :lang() requires a parameter -> ignoring'.format(**condition))
+            return ['False']
+
+        return ["parameters['lang'] == " + repr(condition['value'])]
