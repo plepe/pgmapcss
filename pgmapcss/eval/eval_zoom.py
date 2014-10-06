495815a1c7ac63f1e06e8a9f4ccadfdde8d7d3b6
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-10-06 07:36:22 +0200

    Eval::zoom: Avoid bug (max_scale can be 0 too)

--- pgmapcss/eval/eval_zoom.py
+++ pgmapcss/eval/eval_zoom.py
@@ -10,7 +10,7 @@ class config_eval_zoom(config_base):
         if 'statement' in prop and 'selector' in prop['statement']:
             if 'min_scale' in prop['statement']['selector'] and prop['statement']['selector']['min_scale'] > 0:
                 max_zoom = int(math.ceil(math.log(3.93216e+08 / prop['statement']['selector']['min_scale'], 2)))
-            if 'max_scale' in prop['statement']['selector'] and prop['statement']['selector']['max_scale'] is not None:
+            if 'max_scale' in prop['statement']['selector'] and prop['statement']['selector']['max_scale'] not in (None, 0):
                 min_zoom = int(math.ceil(math.log(3.93216e+08 / (prop['statement']['selector']['max_scale'] - 0.1), 2)))
 
         return ( { str(i) for i in range(min_zoom, max_zoom + 1) }, self.mutable )
