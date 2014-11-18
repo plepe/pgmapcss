6c91a5afae2a305924d01d0873c88443d8613c46
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-11-18 15:52:16 +0100

    DB/Overpass: remove last remnants of code, the module was cloned from

--- pgmapcss/db/overpass/db.py
+++ pgmapcss/db/overpass/db.py
@@ -15,20 +15,7 @@ class db(default):
 
     def tag_type(self, key, condition, selector, statement):
         if key[0:4] == 'osm:':
-            if key == 'osm:id':
-                return ( 'column', 'id', self.compile_modify_id )
-            elif key == 'osm:user':
-                return ( 'column', 'user_id', self.compile_user_id )
-            elif key == 'osm:user_id':
-                return ( 'column', 'user_id' )
-            elif key == 'osm:version':
-                return ( 'column', 'version' )
-            elif key == 'osm:timestamp':
-                return ( 'column', 'tstamp' )
-            elif key == 'osm:changeset':
-                return ( 'column', 'changeset_id' )
-            else:
-                return None
+            return None
 
         return ( 'overpass', key )
 
