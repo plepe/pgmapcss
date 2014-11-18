692e4a08b988569dd26c7a31ec56c162d37a1a22
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2014-11-18 14:14:07 +0100

    PGCache: documentation

--- pgmapcss/misc/pgcache.py
+++ pgmapcss/misc/pgcache.py
@@ -15,6 +15,11 @@
 #        print(r)
 
 class PGCache:
+### __init__(): initialize a new cache
+# Parameters:
+#   id:        identify cache
+#   read_id:   (boolean) should the id of the object (if possible) should automatically be read (default False)
+#   read_geo:  (boolean) should the geometry of the object (if possible) should automatically be read (default False)
     def __init__(self, id, read_id=False, read_geo=False):
         global PGCaches
         try:
@@ -28,6 +33,11 @@ class PGCache:
         self.read_geo = read_geo
         self.cache_id = len(PGCaches)
 
+### add(): add new data
+# Parameters:
+#   data: some data, will be serialized (and on return unserialized)
+#   id: (optional, string) identify this object. If read_id is True and data a dict with a key 'id', this will be used
+#   geo: (optional, geometry) geometry of the object. If read_geo is True and data a dict with a key 'geo', this will be used
     def add(self, data, id=None, geo=None):
         import pickle
         try:
@@ -35,13 +45,16 @@ class PGCache:
         except:
             self.plan_add = plpy.prepare('insert into _pgmapcss_PGCache values (\'' + str(self.cache_id).replace("'", "''") + '\', $1, $2, $3)', [ 'bytea', 'text', 'geometry' ])
 
-        if id is None and self.read_id and 'id' in data:
+        if id is None and self.read_id and type(data) is dict and 'id' in data:
             id = data['id']
-        if geo is None and self.read_geo and 'geo' in data:
+        if geo is None and self.read_geo and type(data) is dict and 'geo' in data:
             geo = data['geo']
 
         plpy.execute(self.plan_add, [ pickle.dumps(data), id, geo ])
 
+### get(): a generator function returning data from cache
+# Parameters:
+#   id: (optional, string or list of strings) only return data which matches the id.
     def get(self, id=None):
         import pickle
         if id is None:
@@ -66,9 +79,21 @@ class PGCache:
         for r in cursor:
             yield pickle.loads(r['data'])
 
+### prepare(): prepare a SQL select statement
+# Parameters:
+#   query: (string) a database query containing "{table}" as database source, e.g. "select * from {table} where id=$1"
+#   param_type: (optional, list of type identifiers) parameter types to the database query, references by $1, $2, ... from the query, e.g. ['text']
+# Return:
+#   a plan, which can be passed to execute() or cursor()
     def prepare(self, query, param_type=[]):
         return plpy.prepare(query.replace('{table}', '(select data, id, geo from _pgmapcss_PGCache where cache_id=' + str(self.cache_id).replace("'", "''") + ') t'), param_type)
 
+### execute(): execute a plan and return a list of result rows
+# Parameters:
+#   plan: a plan from prepare()
+#   param: (optional, list) parameters to the database query, e.g. [ 'w1234' ]
+# Return:
+#   a list with all result rows. if 'data' is in the result columns it will be unserialized.
     def execute(self, plan, param=[]):
         import pickle
         ret = []
@@ -80,6 +105,12 @@ class PGCache:
 
         return ret
 
+### cursor(): execute a plan and yield result rows
+# Parameters:
+#   plan: a plan from prepare()
+#   param: (optional, list) parameters to the database query, e.g. [ 'w1234' ]
+# Return:
+#   a generator generating result rows. if 'data' is in the result columns it will be unserialized.
     def cursor(self, plan, param=[]):
         import pickle
         ret = []
@@ -89,7 +120,12 @@ class PGCache:
                 r['data'] = pickle.loads(r['data'])
             yield r
 
-def get_PGCache(id, read_id=False, read_geo=False):
+### get_PGCache(): get an existing cache, will throw exception if it doesn't exist
+# Parameters:
+#   id: id of the cache
+# Return:
+#   return the existing cache
+def get_PGCache(id):
     global PGCaches
     try:
         PGCaches
